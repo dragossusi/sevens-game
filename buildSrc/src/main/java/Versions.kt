f364e1becfbc6a50e3e12fee7fc9c8f68b3a3768
Dragos Rachieru <rachierudragos97@gmail.com>
2021-01-29 18:53:29 +0200

    Updated version

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -21,5 +21,5 @@ object Versions {
     const val kotlin = "1.4.21"
     const val serialization = "1.0.1"
     const val coroutines = "1.4.2"
-    const val app = "1.0.8"
+    const val app = "1.0.9"
 }
